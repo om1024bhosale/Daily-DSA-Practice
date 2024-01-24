6a17155a150aac68d8e25d5376fb4bd286588879
Om Bhosale <127881279+om1024bhosale@users.noreply.github.com>
2024-01-24 08:57:20 +0530

    Create 25-reverse-nodes-in-k-groups.cpp

---
+++ Linked List/25-reverse-nodes-in-k-groups.cpp
@@ -0,0 +1,87 @@
+/*
+Given the head of a linked list, reverse the nodes of the list k at a time, and return the modified list.
+
+k is a positive integer and is less than or equal to the length of the linked list. If the number of nodes is not a multiple of k then left-out nodes, in the end, should remain as it is.
+
+You may not alter the values in the list's nodes, only nodes themselves may be changed.
+
+Example 1:
+
+
+Input: head = [1,2,3,4,5], k = 2
+Output: [2,1,4,3,5]
+Example 2:
+
+
+Input: head = [1,2,3,4,5], k = 3
+Output: [3,2,1,4,5]
+ 
+
+Constraints:
+
+The number of nodes in the list is n.
+1 <= k <= n <= 5000
+0 <= Node.val <= 1000
+ 
+
+Follow-up: Can you solve the problem in O(1) extra memory space?
+
+*/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    void reverse(ListNode* head){
+        ListNode* prev = NULL, *curr = head;
+        while(curr){
+            ListNode* nxt = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = nxt;
+        }
+    }
+
+    ListNode* findKthNode(ListNode* head, int k){
+        int cnt = 1;
+        while(cnt<k and head!=NULL){
+            head = head->next;
+            cnt++;
+        }
+        return head;
+    }
+
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        ListNode* temp = head;
+        ListNode* prevNode = NULL;
+
+        while(temp!=NULL){
+            ListNode* kthNode = findKthNode(temp, k);
+            if(kthNode == NULL){
+                if(prevNode!=NULL) prevNode->next = temp;
+                break;
+            }
+            ListNode* nextNode = kthNode->next;
+            kthNode->next = NULL;
+            reverse(temp);
+            if(temp==head){
+                head = kthNode;
+            }
+            else{
+                prevNode->next = kthNode;
+            }
+            prevNode = temp;
+            temp = nextNode;
+        }
+
+        return head;
+    }
+};
