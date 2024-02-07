165e4c2bf070afe5b533560766ec75b757f5934a
Om Bhosale <127881279+om1024bhosale@users.noreply.github.com>
2024-02-07 18:09:49 +0530

    Update 46-permutations.cpp

--- Recursion/46-permutations.cpp
+++ Recursion/46-permutations.cpp
@@ -56,3 +56,27 @@ public:
         return ans;
     }
 };
+// second approach: optimal
+void permut(vector<int>& nums,int index,int n,vector<vector<int>>& ans)
+    {
+        if(index==n)
+        {
+            ans.push_back(nums);
+            return;
+        }
+        for(int i=index;i<n;i++)
+        {
+            swap(nums[i],nums[index]);
+            permut(nums,index+1,n,ans);
+            swap(nums[i],nums[index]);
+        }
+    
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+      
+        vector<vector<int> >ans;
+        int n=nums.size();
+        permut(nums,0,n,ans);
+        return ans;
+    }
+};
