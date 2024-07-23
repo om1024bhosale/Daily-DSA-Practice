6b1765b6c5bb36d72f1e2d364536ec8dcd468744
Om Bhosale <127881279+om1024bhosale@users.noreply.github.com>
2024-07-23 15:21:06 +0530

    Create 114-flatten-binary-tree-tgo-linkedlist.cpp

---
+++ Binary Tree/114-flatten-binary-tree-tgo-linkedlist.cpp
@@ -0,0 +1,68 @@
+/*
+ link-> https://leetcode.com/problems/flatten-binary-tree-to-linked-list/description/
+
+ Given the root of a binary tree, flatten the tree into a "linked list":
+
+The "linked list" should use the same TreeNode class where the right child pointer points to the next node in the list and the left child pointer is always null.
+The "linked list" should be in the same order as a pre-order traversal of the binary tree.
+ 
+
+Example 1:
+
+
+Input: root = [1,2,5,3,4,null,6]
+Output: [1,null,2,null,3,null,4,null,5,null,6]
+Example 2:
+
+Input: root = []
+Output: []
+Example 3:
+
+Input: root = [0]
+Output: [0]
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 2000].
+-100 <= Node.val <= 100
+ 
+
+Follow up: Can you flatten the tree in-place (with O(1) extra space)?
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void flatten(TreeNode* root) {
+        while(root)
+        {
+        if(!root->left) root = root->right;
+        else
+        {
+            TreeNode* temp = root->left;
+            while(temp->right)
+            {
+                temp = temp->right;
+            }
+            temp->right = root->right;
+            root->right = root->left;
+            root->left = NULL;
+            root = root->right;
+        }
+        
+        }
+       
+
+    }
+};
