78a8837259ab6bdafe0f6ddb47858737bcd883dc
Om Bhosale <127881279+om1024bhosale@users.noreply.github.com>
2024-07-24 16:48:33 +0530

    Create children-sum-in-binary-tree.cpp

---
+++ Binary Tree/children-sum-in-binary-tree.cpp
@@ -0,0 +1,63 @@
+/*
+link -> https://www.geeksforgeeks.org/problems/children-sum-parent/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=hildren-sum-parent
+
+Given a binary tree having n nodes. Check whether all of its nodes have the value equal to the sum of their child nodes. Return 1 if all the nodes in the tree satisfy the given properties, else it return 0.
+
+For every node, data value must be equal to the sum of data values in left and right children. Consider data value as 0 for NULL child.  Also, leaves are considered to follow the property.
+
+Example 1:
+
+Input:
+Binary tree
+       35
+      /   \
+     20  15
+    /  \  /  \
+   15 5 10 5
+Output: 
+1
+Explanation: 
+Here, every node is sum of its left and right child.
+Example 2:
+
+Input:
+Binary tree
+       1
+     /   \
+    4    3
+   /  
+  5    
+Output: 
+0
+Explanation: 
+Here, 1 is the root node and 4, 3 are its child nodes. 4 + 3 = 7 which is not equal to the value of root node. Hence, this tree does not satisfy the given condition.
+Your Task:
+You don't need to read input or print anything. Your task is to complete the function isSumProperty() that takes the root Node of the binary tree as input and returns 1 if all the nodes in the tree satisfy the following properties, else it returns 0.
+
+Expected Time Complexiy: O(n).
+Expected Auxiliary Space: O(Height of the Tree).
+
+Constraints:
+1 <= n <= 105
+1 <= Data on nodes <= 105
+*/
+
+class Solution{
+    public:
+    //Function to check whether all nodes of a tree have the value 
+    //equal to the sum of their child nodes.
+    int isSumProperty(Node *root)
+    {
+     // Add your code here
+     if(!root) return 1;
+     if(!root->left && !root->right) return 1;
+     int left=0,right=0;
+     if(root->left) left  =root->left->data;
+     if(root->right) right = root->right->data;
+     
+    
+     if (root->data != left + right) return 0;
+     return(isSumProperty(root->left) && isSumProperty(root->right));
+     
+    }
+};
