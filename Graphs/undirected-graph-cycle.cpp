9c16fc3ac468309117d4c9ae827fa23b01aefdcc
Om Bhosale <127881279+om1024bhosale@users.noreply.github.com>
2024-08-28 19:40:06 +0530

    Create undirected-graph-cycle.cpp

---
+++ Graphs/undirected-graph-cycle.cpp
@@ -0,0 +1,75 @@
+/*
+link -> https://www.geeksforgeeks.org/problems/detect-cycle-in-an-undirected-graph/1
+
+Given an undirected graph with V vertices labelled from 0 to V-1 and E edges, check whether it contains any cycle or not. Graph is in the form of adjacency list where adj[i] contains all the nodes ith node is having edge with.
+
+Example 1:
+
+Input:  
+V = 5, E = 5
+adj = {{1}, {0, 2, 4}, {1, 3}, {2, 4}, {1, 3}} 
+Output: 1
+Explanation: 
+
+1->2->3->4->1 is a cycle.
+Example 2:
+
+Input: 
+V = 4, E = 2
+adj = {{}, {2}, {1, 3}, {2}}
+Output: 0
+Explanation: 
+
+No cycle in the graph.
+ 
+
+Your Task:
+You don't need to read or print anything. Your task is to complete the function isCycle() which takes V denoting the number of vertices and adjacency list as input parameters and returns a boolean value denoting if the undirected graph contains any cycle or not, return 1 if a cycle is present else return 0.
+
+NOTE: The adjacency list denotes the edges of the graph where edges[i] stores all other vertices to which ith vertex is connected.
+
+ 
+
+Expected Time Complexity: O(V + E)
+Expected Space Complexity: O(V)
+
+
+ 
+
+Constraints:
+1 ≤ V, E ≤ 105
+
+
+*/
+
+class Solution {
+  public:
+    // Function to detect cycle in an undirected graph.
+    
+    bool dfs(int node, int parent, vector<int> adj[], vector<bool> &visited)
+    {
+        visited[node]=1;
+        for(int j=0;j<adj[node].size();j++)
+        {
+            if(parent==adj[node][j])
+            continue;
+            if(visited[adj[node][j]])
+            return 1;
+            if(dfs(adj[node][j],node,adj,visited))
+            return 1;
+        }
+        return 0;
+    }
+    
+    bool isCycle(int V, vector<int> adj[]) {
+        // Code here
+        vector<bool>visited(V,0);
+        for(int i=0;i<V;i++)
+        {
+            if(!visited[i] && dfs(i,-1,adj,visited))
+            return 1;
+            
+        }
+        return 0;
+    }
+};
